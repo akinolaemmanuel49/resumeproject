080e3b0920afefa49fe300c3595989455a0f457c
Akinola Abiodun <akinolaemmanuel49@gmail.com>
2023-08-31 22:25:39 +0100

    Update

--- resumeproject/env_var.py
+++ resumeproject/env_var.py
@@ -1,6 +1,8 @@
 from pydantic import EmailStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+ENV_FILE = ".env.example"
+
 
 class Settings(BaseSettings):
     SECRET_KEY: str
@@ -11,4 +13,4 @@ class Settings(BaseSettings):
     EMAIL_USE_TLS: bool
     DEFAULT_FROM_EMAIL: EmailStr
 
-    # model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=ENV_FILE)
