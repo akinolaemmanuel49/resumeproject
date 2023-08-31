a197ed8a9922fc51d495c65952e65d99d9bc354d
Akinola Abiodun <akinolaemmanuel49@gmail.com>
2023-08-31 18:13:54 +0100

    Add new Token model

--- user/models.py
+++ user/models.py
@@ -1,5 +1,8 @@
+from random import choice
+
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from user.managers import UserManager
@@ -25,8 +28,27 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
 
+class Token(models.Model):
+    token = models.CharField(_("Token"), blank=False, null=False, max_length=8)
+    token_expires = models.DateTimeField(
+        _("Token Expires"), default=timezone.now() + timezone.timedelta(minutes=10)
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def generate(self) -> str:
+        numbers = [str(i) for i in range(10)]
+        token = ""
+
+        for __ in range(8):
+            token += choice(numbers)
+
+        self.token = token
+
+        return token
+
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(_("First Name"), max_length=255)
     last_name = models.CharField(_("Last Name"), max_length=255)
     email = models.EmailField(_("Email Address"), unique=True)
@@ -36,6 +58,8 @@ class Profile(models.Model):
     )
     created_at = models.DateTimeField(_("Created At"), auto_now_add=True)
     modified_at = models.DateTimeField(_("Modified At"), auto_now=True)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.email
